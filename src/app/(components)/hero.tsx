c21da420e833b8223721806bb4f2f297f3bfd2b6
abimbola7 <abimbolababajide6@gmail.com>
2024-10-15 22:25:40 +0100

    bug fix

--- src/app/(components)/hero.tsx
+++ src/app/(components)/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="w-full max-w-3xl mx-auto px-3 sm:px-0">
       <div className=''>
         <h1 
-        className={`${yeseva.className} text-center text-[80px] sm:text-[100px] md:text-[144px] tracking-wider !text-darkViolet leading-tight`}>
+        className={`${yeseva.className} text-center min-[400px]:text-[80px] text-[60px] sm:text-[100px] md:text-[144px] tracking-wider !text-darkViolet leading-tight`}>
           Galaxy
           <span 
           className='uppercase'>
