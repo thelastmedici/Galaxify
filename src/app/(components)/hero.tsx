57e9f6af6cf1ca4708efb21ee175d3be4f9527ef
abimbola7 <abimbolababajide6@gmail.com>
2024-10-18 06:29:34 +0100

    text gradient

--- src/app/(components)/hero.tsx
+++ src/app/(components)/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="w-full max-w-3xl mx-auto px-3 sm:px-0">
       <div className=''>
         <h1 
-        className={`${ sofia.className } text-center min-[400px]:text-[80px] text-[60px] sm:text-[100px] md:text-[144px] tracking-wider !text-darkViolet leading-tight font-bold`}>
+        className={`${ sofia.className } text-center min-[400px]:text-[80px] text-[60px] sm:text-[100px] md:text-[144px] tracking-wider leading-tight font-bold text-gradient`}>
           Galaxy
           <span 
           className='capitalize'>
